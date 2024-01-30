ba9bfa523c930eedc55a6173d4b149a5dbf29e44
Shoda7 <matheus.shoda21@gmail.com>
2024-01-30 20:01:33 -0300

    initial commit

---
+++ Menu.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+            WriteOptions();
+
+            var option = int.Parse(Console.ReadLine());
+            HandleMenuOption(option);
+        }
+
+        public static void DrawScreen()
+        {
+            Extremo(30);
+            Meio(15);
+            Extremo(30);
+        }
+        public static void Extremo(int tam)
+        {
+            Console.Write("+");
+            for (int i = 0; i < tam; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write(Environment.NewLine);
+        }
+        public static void Meio(int tam)
+        {
+            for (int lines = 0; lines <= tam; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write(Environment.NewLine);
+            }
+        }
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("===========");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.WriteLine("Opção: ");
+            Console.SetCursorPosition(11, 10);
+        }
+        public static void HandleMenuOption(int option)
+        {
+            switch (option)
+            {
+
+                case 0:
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                default: Show(); break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EditorHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Menu.Show();
+        }
+    }
+}
